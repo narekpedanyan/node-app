c5652fa7e610856213b91f5f5e31be16b7ef20df
Narek <naropedan@gmail.com>
2023-03-28 21:06:08 +0400

    Some experiments!

---
+++ app.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Form page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" /><button type="submit">Submit</button></form></body>')
+        res.write('</html>');
+        res.end();
+    } else {
+        res.write('<html>');
+        res.write('<head><title>Form page</title></head>');
+        res.write('<body>Some body text</body>');
+        res.write('</html>');
+        res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+});
+
+server.listen(3000);
