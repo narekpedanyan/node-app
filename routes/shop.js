5201e41470bca88cd178806337ec265358176a8b
Narek <naropedan@gmail.com>
2023-06-25 20:02:04 +0400

    Pug added

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,8 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     console.log(adminData.products, 'bozy ');
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    res.render('shop');
+    // res.sendFile(path.join(rootDir, 'views', 'shop.pug'));
 });
 
 module.exports = router;
