9ec447ee97ef640ada4731d1211932c0b0c0c61d
narekpedanyan <narek@corpora.us>
2024-10-24 14:57:16 +0400

    Extracting dynamic params!

--- models/product.js
+++ models/product.js
@@ -29,6 +29,7 @@ module.exports = class Product {
         this.price = price;
     }
     save() {
+        this.id = Math.random().toString();
         getProductsFromFile((products) => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products, null, 2), (err) => {
