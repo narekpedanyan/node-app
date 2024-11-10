55fe597a04301353fdb85cab10e6450edb5b09ee
narekpedanyan <narek@corpora.us>
2024-11-10 23:15:24 +0400

    Getting a single product with where condition

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,41 +1,49 @@
 const Product = require("../models/product");
-const Cart = require("../models/cart")
+const Cart = require("../models/cart");
+
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll()
-        .then(([rows, fieldData]) => {
+    Product.findAll()
+        .then((products) => {
             res.render('shop/product-list', {
-                prods: rows,
+                prods: products,
                 pageTitle: 'All Products',
                 path: '/products',
             });
-        })
-        .catch(err => console.log(err, 'err'));
+        }).catch(err => console.log(err));
 }
 
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
-    Product.findById(prodId)
-        .then(([product]) => {
+    // Product.findAll({ where: { id: prodId } })
+    //     .then(products => {
+    //         res.render('shop/product-detail', {
+    //             product: products[0],
+    //             pageTitle: 'Product Details',
+    //             path: '/products'
+    //         })
+    //     })
+    //     .catch(err => console.log(err, 'err'));
+
+    Product.findByPk(prodId)
+        .then(product => {
             res.render('shop/product-detail', {
-                product: product[0],
+                product: product,
                 pageTitle: 'Product Details',
                 path: '/products'
             })
         })
-        .catch(err => console.log(err, 'err'))
+        .catch(err => console.log(err, 'err'));
 }
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll()
-        .then(([rows, fieldData]) => {
-                console.log(fieldData, 'fieldData');
-                res.render('shop/index', {
-                    prods: rows,
-                    pageTitle: 'Shop',
-                    path: '/',
-                });
-            })
-        .catch(err => console.log(err, 'err'));
+    Product.findAll()
+    .then((products) => {
+        res.render('shop/index', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+        });
+    }).catch(err => console.log(err));
 }
 
 exports.getCart = (req, res, next) => {
